0f3a285c210a107ec61e15532d1aeab2ef92fd4d
SiriusGuru33 <polluxdev33@gmail.com>
2021-12-23 18:36:53 +0100

    Finished v1

---
+++ app/src/main/java/com/smartlocation/imagecropper/MainActivity.java
@@ -0,0 +1,137 @@
+package com.smartlocation.imagecropper;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btBrowse, btReset;
+    ImageView imageView;
+    Uri uri;
+
+    static String storageDir = "media";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btBrowse = findViewById(R.id.bt_browse);
+        btReset = findViewById(R.id.bt_reset);
+        imageView = findViewById(R.id.image_view);
+
+        btBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.startPickImageActivity(MainActivity.this);
+            }
+        });
+
+        btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageBitmap(null);
+
+            }
+        });
+
+        File dir = new File(this.getFilesDir(), storageDir);
+        if (!dir.exists()) {
+            boolean result = dir.mkdir();
+            Log.v("MainActivity", String.valueOf(result));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            Uri imageuri = CropImage.getPickImageResultUri(this, data);
+            if (CropImage.isReadExternalStoragePermissionsRequired(this, imageuri)) {
+                uri = imageuri;
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            } else {
+                startCrop(imageuri);
+            }
+
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                //imageView.setImageURI(result.getUri());
+                Log.d("AAAAA", String.valueOf(result.getUri()));
+                final InputStream imageStream;
+                try {
+                    imageStream = getContentResolver().openInputStream(result.getUri());
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    String encodedImage = encodeImage(selectedImage);
+                    Log.d("AAAAA", encodedImage);
+                    imageView.setImageBitmap(decodeImage(encodedImage));
+
+                    File imageFile = Paths.get(this.getFilesDir().getAbsolutePath(), storageDir, "croppedImg.png").toFile();
+                    try (FileOutputStream out = new FileOutputStream(imageFile)) {
+                        selectedImage.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+                        // PNG is a lossless format, the compression factor (100) is ignored
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(this, "Image Update Successfully !!!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startCrop(Uri imageuri) {
+        CropImage.activity(imageuri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAllowFlipping(true)
+                .setAllowRotation(true)
+                .setAspectRatio(400, 400)
+                .setMultiTouchEnabled(true)
+                .start(this);
+    }
+
+    private String encodeImage(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 60, baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encImage;
+    }
+
+    private Bitmap decodeImage(String encodedImage) {
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+    }
+
+
+}
+
+
